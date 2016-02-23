fbebeeb2b9e9ce408db3a13af91b181c2900d0ef
artemsh <artyomshorin95@ya.ru>
2016-02-23 18:22:47 +0300

    City issue

--- queries.php
+++ queries.php
@@ -2,6 +2,7 @@
     include "main.php";
     if($_POST['suggest'])
     {
+        //query string creation except city_id
         $param = json_decode($_POST['suggest']);
         $fields = "";
         $values = "'";
@@ -10,15 +11,36 @@
             $values = $values.$param->$val."','";
         }
         unset($val);
-
-
+        //connection to db
         $suggest_db = new mysqli("barter", "root", "", "main");
         if ($suggest_db->connect_errno) {
             $result = array( "res" => "Не удалось подключиться:".$suggest_db->connect_error);
             exit();
         }
-        $fields = (substr($fields, 0, -1));
-        $values = (substr($values, 0, -2));
+        //calculation city_id
+        $city = $param->city;
+        $region_id = $param->region;
+        $city_id = 0;
+
+        $cities_query = $suggest_db->query("SELECT name,id FROM cities WHERE region_id='$region_id'");
+        $cities_array = $cities_query->fetch_all(MYSQL_ASSOC);
+
+        foreach($cities_array as $val)
+        {
+            if(strnatcasecmp($val[name],$city) == 0)
+                $city_id = $val[id];
+        }
+        unset($val);
+        if($city_id == 0)
+        {
+            $suggest_db->real_query("INSERT INTO cities (name,region_id) VALUES ('$city','$region_id')");
+            $city_query = $suggest_db->query("SELECT id FROM cities WHERE name='$city' AND region_id='$region_id'");
+            $city_array = $city_query->fetch_all(MYSQL_ASSOC);
+            $city_id = $city_array[0][id];
+        }
+        $fields = $fields."city";
+        $values = $values.$city_id."'";
+        //query to db
 
         if($suggest_db->real_query("INSERT INTO advertisements ($fields) VALUES ($values)"))
             $result = array( "res" => "ok");
@@ -26,5 +48,23 @@
             $result = array( "res" => "not ok");
         $suggest_db->close();
         echo json_encode($result);
+        exit();
+    }
+    if($_POST['region'])
+    {
+        $region_id = json_decode($_POST['region'])->region;
+        $region_db = new mysqli("barter", "root", "", "main");
+        if ($suggest_db->connect_errno) {
+            exit();
+        }
+        $query_cities = $region_db->query("SELECT name,id FROM cities WHERE region_id='$region_id'");
+        $cities_result = $query_cities->fetch_all(MYSQL_ASSOC);
+        $cities_array = array();
+        foreach ($cities_result as $val) {
+            array_push($cities_array, array($val[name],$val[id]));
+        }
+        unset($val);
+        echo json_encode($cities_array);
+        $region_db->close();
         exit();
     }
--- topics_of_barter.php
+++ topics_of_barter.php
@@ -1,35 +1,3 @@
 ﻿<?php
 
-
-$suggest_db = new mysqli("barter", "root", "", "main");
-if ($suggest_db->connect_errno) {
-    $result = array( "res" => "Не удалось подключиться:".$suggest_db->connect_error);
-    exit();
-}
-if($suggest_db->real_query("INSERT INTO advertisements (title, name) VALUES ('1','дебил')"))
-    echo "luck";
-else
-    echo "fuck";
-/*$addRegions = new mysqli("barter", "root", "", "main");
-if($addRegions->connect_errno)
-{
-    echo "fuck";
-    exit();
-}
-$handle = @fopen("tst.txt", "r");
-$buffer = fgets($handle, 4096);
-$addRegions->set_charset("utf8");
-if ($handle) {
-    while (($buffer = fgets($handle, 4096)) !== false) {
-        $region = $addRegions->real_escape_string($buffer);
-        if($addRegions->real_query("INSERT INTO regions (name) VALUES ('$region')"))
-            printf("%s\n","Success");
-        else
-            printf("%s\n","error");
-    }
-    if (!feof($handle)) {
-        echo "Error: unexpected fgets() fail\n";
-    }
-    fclose($handle);
-}*/
 ?>
