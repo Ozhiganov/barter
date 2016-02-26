4b2a0d1c766247d31280ec5bc4c2bdc6af839692
artemsh <artyomshorin95@ya.ru>
2016-02-26 18:08:48 +0300

    merge fix

--- queries.php
+++ queries.php
@@ -12,7 +12,7 @@
         }
         unset($val);
         //connection to db
-        $suggest_db = new mysqli("barter", "root", "", "main");
+        $suggest_db = new mysqli("barter", "root", "", "barter_main");
         if ($suggest_db->connect_errno) {
             $result = array( "res" => "Не удалось подключиться:".$suggest_db->connect_error);
             exit();
@@ -50,7 +50,7 @@
     if($_POST['region'])
     {
         $region_id = json_decode($_POST['region'])->region;
-        $region_db = new mysqli("barter", "root", "", "main");
+        $region_db = new mysqli("barter", "root", "", "barter_main");
         if ($region_db->connect_errno) {
             exit();
         }
@@ -68,7 +68,7 @@
     }
     if($_POST['find']){
         $find_fields = json_decode($_POST['find']);
-        $find_db = new mysqli("barter", "root", "", "main");
+        $find_db = new mysqli("barter", "root", "", "barter_main");
         if ($find_db->connect_errno) {
             exit();
         }
